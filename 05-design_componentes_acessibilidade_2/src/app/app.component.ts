30788baa3b86415ea705062a64449c4ce4db57fb
Marks Duarte <marksduarte@gmail.com>
2022-05-04 15:43:45 -0300

    Aula 01 - Definição de uma API

--- 05-design_componentes_acessibilidade_2/src/app/app.component.ts
+++ 05-design_componentes_acessibilidade_2/src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, TemplateRef } from '@angular/core';
+import {
+  ModalService,
+  ModalRef,
+} from './shared/components/modal/services/modal.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
+  @ViewChild('modal') modalTemplateRef: TemplateRef<any>;
+
   title = 'a11y-p2';
+  firstName = 'Marks';
+  modalRef: ModalRef;
+
+  constructor(private modalService: ModalService) {}
+
+  ngOnInit(): void {}
+
+  public show(): void {
+    this.modalRef = this.modalService.open({
+      templateRef: this.modalTemplateRef,
+      title: 'User Details',
+    });
+  }
 }
---
+++ 05-design_componentes_acessibilidade_2/src/app/shared/components/modal/services/modal.service.ts
@@ -0,0 +1,19 @@
+import { Injectable, TemplateRef } from '@angular/core';
+
+@Injectable()
+export class ModalService {
+  constructor() {}
+
+  public open(config: ModalConfig): ModalRef {
+    return new ModalRef();
+  }
+}
+
+export interface ModalConfig {
+  templateRef: TemplateRef<any>;
+  title: string;
+}
+
+export class ModalRef {
+  public close(): void {}
+}
