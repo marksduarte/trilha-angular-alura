2784bf337f080bac44f467a1368684c71aba34e8
Marks Duarte <marksduarte@gmail.com>
2022-05-17 16:52:06 -0300

    Aula 04 - ajustes no focusTrap

--- 05-design_componentes_acessibilidade_2/src/app/shared/directives/focus-trap/focus-trap.directive.ts
+++ 05-design_componentes_acessibilidade_2/src/app/shared/directives/focus-trap/focus-trap.directive.ts
@@ -1,4 +1,9 @@
-import { AfterViewInit, Directive, ElementRef } from '@angular/core';
+import {
+  AfterViewInit,
+  Directive,
+  ElementRef,
+  HostListener,
+} from '@angular/core';
 
 @Directive({
   selector: '[appFocusTrap]',
@@ -18,5 +23,24 @@ export class FocusTrapDirective implements AfterViewInit {
     this.firstFocusableElement = focusableElements[0];
     this.lastFocusableElement = focusableElements[focusableElements.length - 1];
     this.firstFocusableElement.focus();
+  }
+
+  @HostListener('keydown', ['$event'])
+  public manageTab(event: KeyboardEvent): void {
+    if (event.key !== 'Tab') return;
+
+    if (
+      event.shiftKey &&
+      document.activeElement === this.firstFocusableElement
+    ) {
+      this.lastFocusableElement.focus();
+      event.preventDefault();
+    } else if (
+      !event.shiftKey &&
+      document.activeElement === this.lastFocusableElement
+    ) {
+      this.firstFocusableElement.focus();
+      event.preventDefault();
+    }
   }
 }
