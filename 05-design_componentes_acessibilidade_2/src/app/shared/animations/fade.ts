565c834405592a5e7775517bf5ba965edf496731
Marks Duarte <marksduarte@gmail.com>
2022-05-17 15:13:58 -0300

    Aula 03 - 10. FadeOut

--- 05-design_componentes_acessibilidade_2/src/app/shared/animations/fade.ts
+++ 05-design_componentes_acessibilidade_2/src/app/shared/animations/fade.ts
@@ -3,6 +3,7 @@ import { animate, style, transition, trigger } from '@angular/animations';
 export const fade = trigger('fade', [
   transition(':enter', [
     style({ opacity: 0 }),
-    animate(1000, style({ opacity: 1 })),
+    animate(100, style({ opacity: 1 })),
   ]),
+  transition(':leave', [animate(100, style({ opacity: 0 }))]),
 ]);
