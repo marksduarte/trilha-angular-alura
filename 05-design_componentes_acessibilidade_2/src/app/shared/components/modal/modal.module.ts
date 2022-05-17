66da7fee2b1ef3e2b98386d12ffe1e2b7042e6cc
Marks Duarte <marksduarte@gmail.com>
2022-05-17 17:05:00 -0300

    Aula 04 - Diretiva FocusBack

--- 05-design_componentes_acessibilidade_2/src/app/shared/components/modal/modal.module.ts
+++ 05-design_componentes_acessibilidade_2/src/app/shared/components/modal/modal.module.ts
@@ -3,10 +3,11 @@ import { CommonModule } from '@angular/common';
 import { ModalComponent } from './modal.component';
 import { ModalService } from './services/modal.service';
 import { FocusTrapModule } from '../../directives/focus-trap/focus-trap.module';
+import { FocusBackModule } from '../../directives/focus-back/focus-back.module';
 
 @NgModule({
   declarations: [ModalComponent],
-  imports: [CommonModule, FocusTrapModule],
+  imports: [CommonModule, FocusTrapModule, FocusBackModule],
   exports: [ModalComponent],
   providers: [ModalService],
 })
---
+++ 05-design_componentes_acessibilidade_2/src/app/shared/directives/focus-back/focus-back.directive.ts
@@ -0,0 +1,20 @@
+import { Directive, OnDestroy, OnInit } from '@angular/core';
+
+@Directive({
+  selector: '[appFocusBack]',
+})
+export class FocusBackDirective implements OnInit, OnDestroy {
+  private lastFocusedElement: Element;
+
+  constructor() {}
+
+  public ngOnInit(): void {
+    this.lastFocusedElement = document.activeElement;
+  }
+
+  public ngOnDestroy(): void {
+    if (this.lastFocusedElement) {
+      (this.lastFocusedElement as HTMLElement).focus();
+    }
+  }
+}
