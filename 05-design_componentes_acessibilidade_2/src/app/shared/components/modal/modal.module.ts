7514cf813669b448ec0b7b4621e3c50cfdad8ef8
Marks Duarte <marksduarte@gmail.com>
2022-05-17 16:04:15 -0300

    Aula 04 - Diretiva focusTrap

--- 05-design_componentes_acessibilidade_2/src/app/shared/components/modal/modal.module.ts
+++ 05-design_componentes_acessibilidade_2/src/app/shared/components/modal/modal.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ModalComponent } from './modal.component';
 import { ModalService } from './services/modal.service';
+import { FocusTrapModule } from '../../directives/focus-trap/focus-trap.module';
 
 @NgModule({
   declarations: [ModalComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, FocusTrapModule],
   exports: [ModalComponent],
   providers: [ModalService],
 })
