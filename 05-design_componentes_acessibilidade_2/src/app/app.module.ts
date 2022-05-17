be05f641fa202e6904f363f14f5ff1a6dae47625
Marks Duarte <marksduarte@gmail.com>
2022-05-17 15:05:53 -0300

    Aula 03 - 06. FadeIn

--- 05-design_componentes_acessibilidade_2/src/app/app.module.ts
+++ 05-design_componentes_acessibilidade_2/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { ModalModule } from './shared/components/modal/modal.module';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -7,7 +8,12 @@ import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, ModalModule],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    ModalModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
--- 05-design_componentes_acessibilidade_2/src/app/shared/components/modal/modal.component.ts
+++ 05-design_componentes_acessibilidade_2/src/app/shared/components/modal/modal.component.ts
@@ -1,12 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { fade } from './../../animations/fade';
+import { Component, HostBinding, OnInit } from '@angular/core';
 import { ModalConfig } from './interfaces/modal-config';
 
 @Component({
   selector: 'app-modal',
   templateUrl: './modal.component.html',
   styleUrls: ['./modal.component.scss'],
+  animations: [fade],
 })
 export class ModalComponent implements OnInit {
+  @HostBinding('@fade') fade = true;
+
   public config: ModalConfig;
 
   constructor() {}
